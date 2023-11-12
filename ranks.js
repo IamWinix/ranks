0e249d1de6f046563e948bb38d9610f5886f6115
winix <105449451+WinixMC@users.noreply.github.com>
2023-11-12 10:00:07 -0500

    Update ranks

--- ranks.js
+++ ranks.js
@@ -6,19 +6,19 @@ $(document).ready(function() {
     var perks = "";
 
     if (rankName === "VIP Rank") {
-      cashAppInfo = "Pay $2.00 on Cash App: $IamWinix";
+      payPalInfo = "Pay $2.00 on Paypal: @Lesionv2"";
       perks = "VIP Perks: /depth, /kittycannon, /nick, /pweather, /ptime, /workbench, /anvil, /smithingtable, Sell bonus - 5%, Access to VIP only chat, In-game and discord rank, White text in game, Placed higher on tablist, Premium Daily Reward, VIP only pet, Vote crate luck bonus 2.5%";
     } else if (rankName === "VIP+ Rank") {
-      cashAppInfo = "Pay $3.50 on Cash App: $IamWinix";
+      payPalInfo = "Pay $3.50 on on Paypal: @Lesionv2"";
       perks = "VIP+ Perks: " + "Including VIP perks, /Sell bonus - 7.5%, More auction house slots, VIP+ only pet, Vote crate luck bonus 5%, /anvil, /trash, /feed, /grindstone";
     } else if (rankName === "MVP Rank") {
-      cashAppInfo = "Pay $5.00 on Cash App: $IamWinix";
+      payPalInfo = "Pay $5.00 on Paypal: @Lesionv2"";
       perks = "MVP Perks: " + "Having VIP perks including more ALL VIP+'s perms, /Sell bonus - 10%, MVP only chat, Higher support time, Rank obtainable only items, /fw, /skulls, /head, /enderchest, Gets access to beta tests, MVP only pet, Vote crate luck bonus 7.5%";
     } else if (rankName === "MVP+ Rank") {
-      cashAppInfo = "Pay $7.00 on Cash App: $IamWinix";
+      payPalInfo = "Pay $7.00 on on Paypal: @Lesionv2"";
       perks = "MVP+ Perks: " + "ALL MVP's perms, /Sell bonus - 15%, Rank obtainable only items, MVP+ only pet, Vote crate luck bonus 10%, /msgtoggle, /paytoggle, /tpauto, /rest, /tptoggle";
     } else if (rankName === "MVP++ Rank") {
-      cashAppInfo = "Pay $10.00 on Cash App: $IamWinix";
+      payPalInfo = "Pay $10.00 on Paypal: @Lesionv2";
       perks = "MVP++ Perks: " + "All previous perks, /Sell bonus - 20%, Vote crate luck bonus 20%, MVP++ only pet, **COLOR AND FORMAT IN CHAT**, /smelt, /afk";
     }
 
